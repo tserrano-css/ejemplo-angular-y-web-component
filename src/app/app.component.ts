21a46351a39b33117409098be16a701ae44317dd
Toni Serrano Suarez <tserrano@cambrasabadell.org>
2022-11-10 20:05:04 +0100

    usando web components en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import 'eit-info-overlay/eit-info-overlay.js';
+import '@dile/dile-modal/dile-modal';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-wc';
+  ngopened = false;
+
+  toggleOverlay() {
+    this.ngopened = !this.ngopened;
+  }
+
+  setClosed() {}
+
+  setClosedModal(){
+    this.ngopened = false;
+  }
 }
